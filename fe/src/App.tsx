840bc883fabaa6e360643e7327925f6e124d5a86
Rúben Carreira <ruben_carreira97@hotmail.com>
2023-11-11 09:12:27 +0000

    fe not found page

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import Boilerplate from './Boilerplate';
+import NotFound from './NotFound';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/boilerplate" element={<Boilerplate />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
